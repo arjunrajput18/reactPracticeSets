20216d1ff2c4947598eb55d63f63093270e9acd7
Arjunsingh Rajput <31860250+arjunrajput18@users.noreply.github.com>
2023-04-09 14:55:40 +0530

    Update Todo.jsx

--- src/PracticeSet2/Components/Todo.jsx
+++ src/PracticeSet2/Components/Todo.jsx
@@ -1,16 +1,12 @@
-import React from 'react'
-
-export const Todo = ({todoItems}) => {
-const colorChecker= isCompleted=> isCompleted?"green":"red"
+export const TodoItem = ({ todoItems }) => {
   return (
     <>
-    {todoItems.map(({title,description,isCompleted})=>{
-       return <div style={{color:colorChecker(isCompleted)}}>
-       <h1>title:{title}</h1>
-       <p> description:{description} </p>
+      {todoItems.map(({ id, title, description, isCompleted }) => (
+        <div style={{ color: isCompleted ? "green" : "red" }} key={id}>
+          <h2>{title}</h2>
+          <p> {description}</p>
         </div>
-    })}
-
+      ))}
     </>
-  )
-}
+  );
+};
